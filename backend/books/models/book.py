d16157cba1906617b0e0daf0efd6e1e0061d6e84
DESKTOP-INOBOOA\Piotr Tobiasz <tobiv1337@gmail.com>
2020-10-13 17:41:07 +0200

    Setup books api

--- backend/books/models/book.py
+++ backend/books/models/book.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 
 class Book(models.Model):
     title = models.TextField()
     author = models.TextField()
     publish_date = models.DateField()
-    isbn = models.IntegerField()
+    isbn = models.TextField(max_length=13, validators=[MinLengthValidator(10)])
     pages_number = models.PositiveIntegerField()
     cover_link = models.TextField()
     language = models.TextField()
